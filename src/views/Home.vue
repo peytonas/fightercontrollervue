23380b4045775baef3c6a3023d69b27c2a37b5c4
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-09-11 15:54:53 -0600

    pics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="fight" src="../assets/stickeroid_5bf5459e293ac.png" class="img" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+
+<style>
+.img {
+  height: 150px;
+}
+</style>
